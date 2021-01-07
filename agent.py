06ec9caf6bef41427a543f842df3b641b1d0958f
datctbk <datctbk@gmail.com>
2021-01-07 09:47:22 +0700

    First commit

---
+++ agent.py
@@ -0,0 +1,53 @@
+import numpy as np
+from collections import defaultdict
+
+class Agent:
+
+    def __init__(self, nA=6):
+        """ Initialize agent.
+
+        Params
+        ======
+        - nA: number of actions available to the agent
+        """
+        self.nA = nA
+        self.Q = defaultdict(lambda: np.zeros(self.nA))
+        self.alpha = 1.0
+        self.epsilon = 0.9
+        self.gamma = 0.95
+        self.decay_alpha = 10e-7
+        self.decay = 10e-5
+    def select_action(self, state):
+        """ Given the state, select an action.
+
+        Params
+        ======
+        - state: the current state of the environment
+
+        Returns
+        =======
+        - action: an integer, compatible with the task's action space
+        """
+        self.epsilon = self.epsilon - self.decay * self.epsilon
+        self.alpha = max(0.0001,self.alpha - self.alpha*self.decay_alpha)
+        #print(self.alpha)
+        if np.random.random()<self.epsilon:
+            return np.random.choice(self.nA)
+        else:
+            return np.argmax(self.Q[state])
+
+    def step(self, state, action, reward, next_state, done):
+        """ Update the agent's knowledge, using the most recently sampled tuple.
+
+        Params
+        ======
+        - state: the previous state of the environment
+        - action: the agent's previous choice of action
+        - reward: last reward received
+        - next_state: the current state of the environment
+        - done: whether the episode is complete (True or False)
+        """
+        current = self.Q[state][action]
+        target = reward + self.gamma*np.max(self.Q[next_state])
+        td_error = target - current
+        self.Q[state][action] += self.alpha * td_error
